3ec192fb3ceeb64d11b11a47aafa0412c9531a14
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2024-01-10 22:43:58 +0530

    Added shopping cart

--- src/Components/FilteredProducts/SingleProduct.jsx
+++ src/Components/FilteredProducts/SingleProduct.jsx
@@ -128,6 +128,8 @@ function SingleProduct() {
                           addToCart({
                             id: item.id,
                             name: item.name,
+                            img: item.img,
+                            text: item.text,
                             size: size,
                             color: color,
                             price: item.price,
