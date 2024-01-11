f12bbd633676bf33deba97603bd09c78b522323f
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2024-01-11 14:40:04 +0530

    filltered products

--- src/Components/FilteredProducts/FilteredProducts.jsx
+++ src/Components/FilteredProducts/FilteredProducts.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "./ProductCard";
+import { Button } from "@material-tailwind/react";
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+} from "@material-tailwind/react";
+import Error from "../Error/Error";
+import {
+  filterGender,
+  filterByColor,
+  filterBySize,
+  sortByPrice,
+  filterProducts,
+  filterByShoeSize,
+} from "../../features/slices/productsSlice";
 
 function FilteredProducts() {
   const products = useSelector((state) => state.products.filteredProducts);
+  const error = useSelector((state) => state.products.error);
   console.log("products", products);
   const { type } = useParams();
-  console.log("params", type);
+  const genderButtons = ["male", "female"];
+  const colorButtons = [
+    "red",
+    "green",
+    "purple",
+    "yellow",
+    "orange",
+    "blue",
+    "black",
+    "brown",
+  ];
+  const sizeButtons = ["S", "M", "L", "XL"];
+  const shoesizeButtons = ["36", "38", "40", "42", "44", "46"];
+  const dispatch = useDispatch();
+
   return (
     <div>
       <div className="pt-16">
@@ -15,25 +46,159 @@ function FilteredProducts() {
           <h1 className="text-4xl font-inter text-gray-600 font-bold tracking-normal leading-none">
             {type}
           </h1>
+
+          <div className="flex items-center justify-between py-8">
+            <div className="flex items-center">
+              {genderButtons.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <Button
+                      color="gray"
+                      size="lg"
+                      variant="outlined"
+                      ripple={true}
+                      className="text-black hover:bg-gray-300 duration-300 ease-in-out mr-4"
+                      onClick={() => dispatch(filterGender(item))}
+                    >
+                      {item}
+                    </Button>
+                  </div>
+                );
+              })}
+              <Button
+                color="gray"
+                size="lg"
+                variant="outlined"
+                ripple={true}
+                className="text-black hover:bg-gray-300 duration-300 ease-in-out mr-4"
+                onClick={() => dispatch(sortByPrice())}
+              >
+                High Price
+              </Button>
+              <Menu>
+                <MenuHandler>
+                  <Button
+                    color="gray"
+                    size="lg"
+                    variant="outlined"
+                    ripple={true}
+                    className="text-black hover:bg-gray-300 duration-300 ease-in-out mr-4"
+                  >
+                    Select a color
+                  </Button>
+                </MenuHandler>
+                <MenuList>
+                  {colorButtons.map((item, index) => {
+                    return (
+                      <MenuItem
+                        style={{ color: item }}
+                        key={index}
+                        onClick={() => dispatch(filterByColor(item))}
+                      >
+                        {item}
+                      </MenuItem>
+                    );
+                  })}
+                </MenuList>
+              </Menu>
+              <Menu>
+                <MenuHandler>
+                  <Button
+                    disabled={type === "Bags" || type === "Shoes"}
+                    color="gray"
+                    size="lg"
+                    variant="outlined"
+                    ripple={true}
+                    className="text-black hover:bg-gray-300 duration-300 ease-in-out mr-4"
+                  >
+                    Select a size
+                  </Button>
+                </MenuHandler>
+                <MenuList>
+                  {sizeButtons.map((item, index) => {
+                    return (
+                      <MenuItem
+                        key={index}
+                        onClick={() => dispatch(filterBySize(item))}
+                      >
+                        {item}
+                      </MenuItem>
+                    );
+                  })}
+                </MenuList>
+              </Menu>
+              <Menu>
+                <MenuHandler>
+                  <Button
+                    disabled={
+                      type === "Bags" ||
+                      type === "Hoodies" ||
+                      type === "Dresses" ||
+                      type === "Suits" ||
+                      type === "T-Shirts" ||
+                      type === "Jeans" ||
+                      type === "Jackets"
+                    }
+                    color="gray"
+                    size="lg"
+                    variant="outlined"
+                    ripple={true}
+                    className="text-black hover:bg-gray-300 duration-300 ease-in-out mr-4"
+                  >
+                    Select a shoe size
+                  </Button>
+                </MenuHandler>
+                <MenuList>
+                  {shoesizeButtons.map((item, index) => {
+                    return (
+                      <MenuItem
+                        key={index}
+                        onClick={() => dispatch(filterByShoeSize(item))}
+                      >
+                        {item}
+                      </MenuItem>
+                    );
+                  })}
+                </MenuList>
+              </Menu>
+            </div>
+            <div className="pr-14">
+              <Button
+                className="text-black hover:bg-gray-300 duration-300 ease-in-out mr-4"
+                color="gray"
+                size="lg"
+                variant="outlined"
+                ripple={true}
+                onClick={() => dispatch(filterProducts(type))}
+              >
+                {" "}
+                Clear Filter
+              </Button>
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-4 justify-items-center py-8 gap-12">
-          {products
-            .filter((product) => product.type === type)
-            .map((product, index) => {
-              return (
-                <div key={index}>
-                  <ProductCard
-                    id={product.id}
-                    name={product.name}
-                    text={product.text}
-                    img={product.img}
-                    price={product.price}
-                    colors={product.color}
-                  />
-                </div>
-              );
-            })}
-        </div>
+        {error ? (
+          <Error />
+        ) : (
+          <div className="grid grid-cols-4 justify-items-center py-8 gap-12">
+            {products
+              .filter((product) => product.type === type)
+              .map((product, index) => {
+                return (
+                  <div key={index}>
+                    <ProductCard
+                      id={product.id}
+                      name={product.name}
+                      text={product.text}
+                      img={product.img}
+                      price={product.price}
+                      colors={product.color}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        )}
       </div>
     </div>
   );
